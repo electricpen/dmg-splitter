3b9b2205531894e86aa96858f420c0ad5ae78fac
Jonathan Ling <electric.pen@gmail.com>
2017-12-07 23:25:46 -0800

    Initialize application

---
+++ src/characterClass.js
@@ -0,0 +1,17 @@
+class character {
+  constructor(name, hp, dr) {
+    this.name = name;
+    this.totalHP = hp;
+    this.currentHP = hp;
+    this.dr = dr;
+    this.shareList = [];
+  }
+
+  setDR(dr) {
+    this.dr = dr;
+  }
+
+  addShareTarget(name) {
+    this.shareList.push(name);
+  }
+}
